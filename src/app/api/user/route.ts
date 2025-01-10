406dc48e7b4d232d56a787dad5b490891c65e290
dips <dipeshshrestha910@gmail.com>
2025-01-10 19:23:08 +0545

    lets gooo just fixed clerk auth with db happy af

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -4,7 +4,7 @@ import prisma from '@/lib/prisma';
 
 
 export async function GET() {
-  const { userId } = auth();
+  const { userId } = await auth();
   if (!userId) {
     return new NextResponse('Unauthorized', { status: 401 });
   }
